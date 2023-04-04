aa3032d4f303c6902e8faf1f33b500853edc754a
IzykPringle <ijpringl@mtu.edu>
2023-04-04 13:31:15 -0600

    add bombs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,79 +2,124 @@ import react, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  
-  
+
+
   const [size, setSize] = useState(10);
-  const [board, setBoard] = useState(Array.from({length: size},()=> Array.from({length: size}, () => 1)));
-  const [bombTotal, setBombTotal] = useState(10);
-  
-  
+  const [board, setBoard] = useState(Array.from({ length: size }, () => Array.from({ length: size }, () => 1)));
+  const [bombTotal, setBombTotal] = useState(20);
+
+
   useEffect(() => {
-    initializeBoard()
+    initializeBoard();
     addBombs();
-    
+
   }, [])
   class Cell {
-    constructor() {
+    constructor(row, column) {
       this.isClicked = false;
       this.isBomb = false;
       this.bombsNear = 0;
       this.mask = "_";
+      this.row = row;
+      this.column = column;
     }
     getIsBomb() {
       return this.isBomb
     }
-    
+
     setIsBomb(v) {
       this.isBomb = v;
     }
 
+    getBombsNear() {
+      return this.bombsNear;
+    }
+
+    setBombsNear() {
+      this.bombsNear++;
+    }
+
     handleClick() {
+      let tempboard = [...board];
       this.isClicked = true;
       if (this.isBomb) {
         this.mask = '💣';
+        alert("game over");
+        window.location.reload();
       } else {
         this.mask = this.bombsNear;
+        if (tempboard[this.row + 1] !== undefined && tempboard[this.row + 1].bombsNear == 0) {
+          tempboard[this.row + 1][this.column].handleClick();
+        }
+        if (tempboard[this.row - 1] !== undefined && tempboard[this.row - 1].bombsNear == 0) {
+          tempboard[this.row - 1][this.column].handleClick();
+        }
+        if (tempboard[this.row][this.column - 1] !== undefined && tempboard[this.row][this.column - 1].bombsNear == 0) {
+          tempboard[this.row][this.column - 1].handleClick();
+        }
+        if (tempboard[this.row][this.column + 1] !== undefined && tempboard[this.row][this.column + 1].bombsNear == 0) {
+          tempboard[this.row][this.column + 1].handleClick();
+        }
+      }
+      tempboard[this.row][this.column] = this;
+      setBoard(tempboard);
+    }
+  }
+
+  function initializeBoard() {
+    let tempboard = [...board];
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        tempboard[i][j] = new Cell(i, j);
       }
     }
+    setBoard(tempboard);
+    console.log(board);
   }
-  
-  function initializeBoard() {
-    let tempboard=[...board];
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      tempboard[i][j] = new Cell(false, false, 0);
-    }
-  }
-  setBoard(tempboard);
-}
-  
+
   function addBombs() {
-    let counter = bombTotal;
-    let tempboards = [...board];
-    for (let i = 0; i < 100; i++) {
-      for (let j in counter) {
-  tempboards[i][j].setIsBomb(true);
+    let tempboard = [...board];
+    let count = 0;
+
+    while (count < bombTotal) {
+      let randx = Math.floor(Math.random() * size);
+      let randy = Math.floor(Math.random() * size)
+      tempboard[randx][randy].setIsBomb(true);
+      if (tempboard[randx + 1] !== undefined) {
+        tempboard[randx + 1][randy].setBombsNear();
+      }
+      if (tempboard[randx - 1] !== undefined) {
+        tempboard[randx - 1][randy].setBombsNear();
+      }
+      if (tempboard[randx][randy - 1] !== undefined) {
+        tempboard[randx][randy - 1].setBombsNear();
+      }
+      if (tempboard[randx][randy + 1] !== undefined) {
+        tempboard[randx][randy + 1].setBombsNear();
+      }
+    
+      
+      count++;
     }
+
+    setBoard(tempboard);
   }
-  setBoard(tempboards)
-  console.log(board)
-  }
-  
-  
+
+
+
   return (
     <>
-    <div id='boardgrid'>
-{board.map((row) => {
-  
-  return row.map((cell, ind) => {
-return <div key={ind} id='cell' onClick={() =>{cell.handleClick()}}>{cell.mask}</div>
-})
-})}
+      <div id='boardgrid'>
+        {board.map((row) => {
+
+          return row.map((cell, ind) => {
+            return <div key={ind} id='cell' onClick={() => { cell.handleClick() }}>{cell.mask}</div>
+          })
+        })}
 
 
 
-</div>
+      </div>
     </>
   );
 }
